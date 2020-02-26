68d6778e653d382448788ba6e41e68e32ed490e3
Roman Gorelik <rgorelik@reonomy.com>
2020-02-25 23:09:20 -0500

    Change how the Buttons component works to allow more freedom for the designers

--- src/content/buttons/index.tsx
+++ src/content/buttons/index.tsx
@@ -1,78 +1,65 @@
 import React, { useState } from 'react'
-import { withStyles } from '@material-ui/styles'
-import styles, { IStyleClasses } from './styles';
-
-import Button from '@material-ui/core/Button';
-import DeleteIcon from '@material-ui/icons/Delete';
+import { makeStyles } from '@material-ui/styles'
 import CodeIcon from '@material-ui/icons/Code';
 import Highlight from 'react-highlight'
 import buttonString from './string';
 import '../../../node_modules/highlight.js/styles/tomorrow-night-bright.css'
+import jsxToString from 'jsx-to-string';
 
-
-const Buttons = ({ classes }: IStyleClasses) => {
-    const [code, setCode] = useState(false)
-
-    return (
-        <section className={classes.root}>
-            <div className={classes.buttons}>
-            <CodeIcon className={classes.expand} fontSize='small' onClick={() => setCode(!code)}></CodeIcon>
-            <Button 
-             size="medium"
-             variant="contained"
-             color="primary"
-            >
-                Primary
-            </Button>
-
-            <Button 
-             size="medium"
-             variant="contained"
-             color="secondary"
-            >
-                Secondary
-            </Button>
-
-            <Button 
-             size="medium"
-             variant="contained"
-             disabled
-            >
-                Disabled
-            </Button>
-
-            <Button 
-             size="medium"
-             variant="outlined"
-             color="primary"
-            >
-                Light
-            </Button>
-
-            <Button 
-             size="medium"
-             variant="outlined"
-             color="primary"
-            >
-                Outlined
-            </Button>
-
-            <Button size="medium" color="primary">Text</Button>
-
-            <Button
-             color="primary"
-             startIcon={<DeleteIcon />}
-             className={classes.margin}
-            >
-                Icon and Text
-            </Button>
-            </div>
-            {code && 
-            <Highlight language="jsx" className={classes.code}>
-                {buttonString}
-            </Highlight>}
-        </section>
-    )
+interface IButtons {
+  children: React.ReactElement;
 }
 
-export default withStyles(styles)(Buttons)
+const useStyles = makeStyles({
+  root: {
+    
+  },
+  button: {
+    display: 'flex',
+    justifyContent: 'space-between' as 'space-between',
+    padding: `16px 8px`,
+    paddingRight: '50px',
+    background: `#F7F9FE`,
+    position: 'relative' as 'relative'
+  },
+  expand: {
+    position: 'absolute' as 'absolute',
+    top: 0,
+    right: 0,
+    cursor: 'pointer'
+  },
+  code: {
+    padding: `16px`,
+    fontSize: `14px`
+  }
+});
+
+const Buttons = (props: IButtons) => {
+  const classes = useStyles();
+  const [code, setCode] = useState(false)
+  const children = React.Children.toArray(props.children)
+
+  let stringChildren: any = []
+
+  for (let i = 0; i < React.Children.count(children); i++) {
+    stringChildren.push(jsxToString(props.children[i]))
+  }
+
+  stringChildren = stringChildren.join(' ')
+  
+  return (
+    <section className={classes.root}>
+      <div className={classes.button}>
+        <CodeIcon className={classes.expand} fontSize='small' onClick={() => setCode(!code)}></CodeIcon>
+        {props.children}
+      </div>
+      {code && 
+        <Highlight language="javascript" className={classes.code}>
+            {buttonString}
+        </Highlight>} 
+    </section>
+  )
+}
+
+
+export default Buttons
